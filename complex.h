cf05db3d4aa4c4cd48caa2549bdf40d581bdfa20
Tobiasz Filar <tobiasz@MacBook-Pro-Tobiasz.local>
2018-08-26 10:18:23 +0200

    minimal update

--- complex.h
+++ complex.h
@@ -1,5 +1,4 @@
-#ifndef COMPLEX_H
-#define COMPLEX_H
+#pragma once
 
 #include <math.h>
 #include <iostream>
@@ -9,37 +8,42 @@ namespace cmpx {
     class Complex
     {
     public:
-        constexpr           Complex(const Type& real = {}, const Type& imaginary = {})   noexcept;
-                            ~Complex()                                                 = default;
+        constexpr Complex(const Type& real = {}, const Type& imaginary = {}) noexcept;
+        ~Complex() = default;
 
-        constexpr           Complex(Complex&&)                                         = default;
-        constexpr           Complex& operator =(Complex&&)                             = default;
+        constexpr Complex(Complex&&) = default;
+        constexpr Complex& operator =(Complex&&) = default;
 
-        constexpr           Complex(const Complex&)                                    = default;
-        constexpr           Complex& operator =(const Complex&)                        = default;
+        constexpr Complex(const Complex&) = default;
+        constexpr Complex& operator =(const Complex&) = default;
 
-        auto                operator +(const Complex&)                                   const noexcept;
-        auto&               operator +=(const Complex&)                                  noexcept;
-        auto                operator -(const Complex&)                                   const noexcept;
-        auto&               operator -=(const Complex&)                                  noexcept;
-        auto                operator *(Type number)                                      const noexcept;
-        auto&               operator *=(Type number)                                     noexcept;
+        auto operator +(const Complex&) const noexcept;
+        auto& operator +=(const Complex&) noexcept;
+        auto operator -(const Complex&) const noexcept;
+        auto& operator -=(const Complex&) noexcept;
+        auto operator *(Type) const noexcept;
+        auto& operator *=(Type) noexcept;
+        auto operator *(const Complex<Type>&) const noexcept;
 
-        constexpr bool      operator ==(const Complex&)                                  const noexcept;
-        constexpr bool      operator !=(const Complex&)                                  const noexcept;
+        constexpr bool operator ==(const Complex&) const noexcept;
+        constexpr bool operator !=(const Complex&) const noexcept;
 
-        constexpr Type      realNumber()                                                 const noexcept { return realNum;}
-        constexpr Type      imaginaryNumber()                                            const noexcept { return imaginaryNum;}
-        constexpr double    module()                                                     const noexcept;
-        constexpr auto      operator -()                                                 const noexcept;
+        constexpr Type realNumber() const noexcept {
+            return realNum;
+        }
+        constexpr Type imaginaryNumber() const noexcept {
+            return imaginaryNum;
+        }
+        constexpr double module() const noexcept;
+        constexpr auto operator -() const noexcept;
 
-        auto&               print(std::ostream& os)                                      const;
-        auto&               scanner(std::istream&);
+        auto& print(std::ostream& os) const;
+        auto& scanner(std::istream&);
 
     private:
-        Type                realNum;
-        Type                imaginaryNum;
-        char                sign;
+        Type realNum;
+        Type imaginaryNum;
+        char sign;
     };
 
     template <typename Type>
@@ -83,6 +87,13 @@ namespace cmpx {
         realNum *= number;
         imaginaryNum *= number;
         return *this;
+    }
+
+    template <typename Type>
+    auto Complex<Type>::operator *(const Complex<Type>& c) const noexcept {
+        return Complex<Type>{
+            realNum * c.realNum, imaginaryNum * c.imaginaryNum
+        };
     }
 
     template <typename Type>
@@ -130,4 +141,3 @@ namespace cmpx {
         return !(*this == c);
     }
 }
-#endif // COMPLEX_H
